37d561653380be5712b2b935561f25e3223dd938
keneth hibaya <k.hibaya.143368.tc@umindanao.edu.ph>
2025-09-19 09:44:03 +0800

    hibaya done frontend in rooms and offices

--- um-ai-chat-admin/src/tabs/professor.tsx
+++ um-ai-chat-admin/src/tabs/professor.tsx
@@ -3,7 +3,7 @@ export default function professor(){
         <>
         <div className="bg-[#3C3C3C] w-[1100px] h-[800px] mt-[-50] pt-10">
     <div className="flex justify-center justify-self-center text-[32px] font-bold bg-[#900C27] rounded-full w-[250px] h-[50px] ">
-      <h1 className="">Rules</h1>
+      <h1 className="">Professor</h1>
     </div>
       <div className="w-[1025px] h-[650px]  mt-12 mx-10 flex-col ">
         <input type="text" className="w-[100px] mr-2 text-black" placeholder="name" />
